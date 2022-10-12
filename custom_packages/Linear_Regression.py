9d0ab6990aeb95b690f8f7d573d0053fdd021fc8
Tyler Zimmer <tyler.zimmer@chevron.com>
2022-10-11 22:38:07 -0500

    full summary table in lin reg!

--- custom_packages/Linear_Regression.py
+++ custom_packages/Linear_Regression.py
@@ -2,7 +2,55 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+from sklearn import linear_model
+from scipy import stats
+import numpy as np
 
+
+########################################
+# Extend sklearn.linear_model
+########################################
+
+class LinearRegression(linear_model.LinearRegression):
+    """
+    LinearRegression class after sklearn's, but calculate t-statistics
+    and p-values for model coefficients (betas).
+    Additional attributes available after .fit()
+    are `t` and `p` which are of the shape (y.shape[1], X.shape[1])
+    which is (n_features, n_coefs)
+    This class sets the intercept to 0 by default, since usually we include it
+    in X.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if not "fit_intercept" in kwargs:
+            kwargs['fit_intercept'] = True
+        super(LinearRegression, self)\
+                .__init__(*args, **kwargs)
+
+    def fit(self, X, y, n_jobs=1):
+        self = super(LinearRegression, self).fit(X, y, n_jobs)
+
+        sse = np.sum((self.predict(X) - y) ** 2, axis=0) / float(X.shape[0] - X.shape[1])
+        se = np.array([
+            np.sqrt(np.diagonal(sse[i] * np.linalg.inv(np.dot(X.T, X))))
+                                                    for i in range(sse.shape[0])
+                    ])
+
+        self.se = se
+        self.t = self.coef_ / se
+        self.p = 2 * (1 - stats.t.cdf(np.abs(self.t), y.shape[0] - X.shape[1]))
+        return self
+
+########################################
+# Data Collation for Summary Table
+########################################
+
+def collate_values(column):
+    list_of_stats = np.concatenate(column)
+    list_of_stats = np.append(0, list_of_stats)
+    list_of_stats = np.round(list_of_stats, decimals=2)
+    return list_of_stats
 
 ########################################
 # Summary Table
@@ -17,16 +65,28 @@ def summary_table(regressor, column_names):
     coefficients=[]
     coefficients.extend(regressor.intercept_.tolist())
     coefficients.extend(regressor.coef_[0].tolist())
+    coefficients = np.round(coefficients, decimals=3)
     
+    # Standard Error
+    standard_errors = collate_values(regressor.se)
     # T Stat
-    
+    t_stats = collate_values(regressor.t)
     # P Value
-    
+    p_values = collate_values(regressor.p)
     # VIF
     
     # Confidence Intervals
 
-    summary_table = pd.DataFrame({'Feature Name': feature_names, 'Coefficient': coefficients})
+    #coefficient - 1.96 * se
+
+    #coefficient + 1.96 * se
+
+    summary_table = pd.DataFrame({'Feature Name': feature_names, 
+                                   'Coefficient': coefficients,
+                                'Standard Error': standard_errors, 
+                                   "T Statistic": t_stats, 
+                                       "P Value": p_values,
+                                   })
     
     return summary_table
 
--- custom_packages/__init__.py
+++ custom_packages/__init__.py
@@ -5,4 +5,4 @@ from .Continuous_Model import accuracy_table, residuals_vs_fitted
 from .Categorical_Model import confusion_matrix, feature_importance, categorical_accuracy_table
 
 # Import linear regression custom packages
-from .Linear_Regression import summary_table, create_diagnostic_plots
+from .Linear_Regression import summary_table, create_diagnostic_plots, LinearRegression
